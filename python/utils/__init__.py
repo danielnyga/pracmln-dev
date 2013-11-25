a041837fe660da05c92e5df60a7beb4a841eaefd
Daniel Nyga <nyga@cs.uni-bremen.de>
2013-11-25 15:00:56 +0100

    Introduced decorator for deprecated functions.

--- python/utils/__init__.py
+++ python/utils/__init__.py
@@ -1,3 +1,4 @@
+import logging
 
 def combinations(domains):
     if len(domains) == 0:
@@ -11,6 +12,20 @@ def _combinations(domains, comb):
     for v in domains[0]:
         for ret in _combinations(domains[1:], comb + [v]):
             yield ret
+            
+def deprecated(func):
+    '''
+    This is a decorator which can be used to mark functions
+    as deprecated. It will result in a warning being emmitted
+    when the function is used.
+    '''
+    def newFunc(*args, **kwargs):
+        logging.getLogger().warning("Call to deprecated function: %s." % func.__name__)
+        return func(*args, **kwargs)
+    newFunc.__name__ = func.__name__
+    newFunc.__doc__ = func.__doc__
+    newFunc.__dict__.update(func.__dict__)
+    return newFunc
             
 def unifyDicts(d1, d2):
     '''
