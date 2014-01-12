020e00845c161778a4227e7db507d05a1e4fe812
Daniel Nyga <nyga@cs.uni-bremen.de>
2014-01-12 17:16:50 +0100

    Renamed Gibbs Sampler module.

--- python/mln/inference/__init__.py
+++ python/mln/inference/__init__.py
@@ -1,7 +1,7 @@
 
 from exact import ExactInference, ExactInferenceLinear, EnumerationAsk
 from mcsat import MCSAT, SampleSAT, FuzzyMCSAT
-from GibbsSampler import GibbsSampler
+from gibbs import GibbsSampler
 from ipfpm import IPFPM
 from SAMaxWalkSAT import SAMaxWalkSAT
 from wcspinfer import WCSPInference
