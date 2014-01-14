854d033f683acce2118a2000b9879fbe895a6ba7
Daniel Nyga <nyga@cs.uni-bremen.de>
2014-01-14 21:23:37 +0100

    Fixed import.

--- python/mln/grounding/default.py
+++ python/mln/grounding/default.py
@@ -21,7 +21,7 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from AbstractGrounding import AbstractGroundingFactory
+from common import AbstractGroundingFactory
 import logging
 from mln.util import strFormula
 from logic.common import Logic
