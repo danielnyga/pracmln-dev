02da9ba1f53cecdcf526f92e6d4fa5a9af6b4b69
Daniel Nyga <nyga@cs.uni-bremen.de>
2014-10-29 11:30:17 +0100

    cleaned up WCSP converter code

--- python/wcsp/converter.py
+++ python/wcsp/converter.py
@@ -21,17 +21,16 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-import bisect
 from wcsp import WCSP
 from wcsp import Constraint
 import utils
 from praclog import logging
-from utils import deprecated
 from logic.common import Logic
 from mln.database import Database
-import sys
 import copy
 from mln.atomicblocks import MutexBlock, SoftMutexBlock
+
+
 
 class WCSPConverter(object):
     '''
@@ -41,13 +40,8 @@ class WCSPConverter(object):
     
     def __init__(self, mrf):
         self.mrf = mrf
-        self.mln = mrf.mln
-        self.vars = []
-        self.varIdx2GndAtom = {}
-        self.gndAtom2VarIndex = {}
-#         self.mutexVars = set() # hold indices of mutex variables
+        self.gndAtom2AtomicBlock = {}
         self.createVariables()
-#         self.simplifyVariables()
         self.constraintBySignature = {}
     
     
@@ -77,185 +71,48 @@ class WCSPConverter(object):
                 self.variables.append(newAtomicBlock)
                 for gndatom in newAtomicBlock.gndatoms:
                     self.gndAtom2AtomicBlock[gndatom.idx] = newAtomicBlock
-#         for block in self.variables:
-#             print block
-#             for val in block.generateValueTuples():
-#                 print val 
-            
-#         handledBlocks = set()
-#         for gndAtom in self.mrf.gndAtoms.values():
-#             blockName = self.mrf.gndBlockLookup.get(gndAtom.idx, None)
-#             varIdx = len(self.vars)
-#             if blockName is not None:
-#                 if blockName not in handledBlocks:
-#                     # create a new variable
-#                     self.vars.append(blockName)
-#                     # create the mappings
-#                     for gndAtomIdx in self.mrf.gndBlocks[blockName]:
-#                         self.gndAtom2VarIndex[self.mrf.gndAtomsByIdx[gndAtomIdx]] = varIdx
-#                     self.varIdx2GndAtom[varIdx] = [self.mrf.gndAtomsByIdx[i] for i in self.mrf.gndBlocks[blockName]]
-#                     handledBlocks.add(blockName)
-#                     self.mutexVars.add(varIdx)
-#             else:
-#                 self.vars.append(str(gndAtom))
-#                 self.gndAtom2VarIndex[gndAtom] = varIdx
-#                 self.varIdx2GndAtom[varIdx] = [gndAtom]
-        
-#     def simplifyVariables(self):
-#         '''
-#         Removes variables that are already given by the evidence.
-#         '''
-#         log = logging.getLogger(self.__class__.__name__)
-#         sf_varIdx2GndAtoms = {}
-#         sf_gndAtom2VarIdx = {}
-#         sf_vars = []
-#         sf_mutexVars = set()
-#         evidence = [i for i, e in enumerate(self.mrf.evidence) if e is not None]
-#         for varIdx, var in enumerate(self.vars):
-#             gndAtoms = filter(lambda x: x.idx not in evidence, self.varIdx2GndAtom[varIdx])
-#             if len(gndAtoms) > 0: # not all gndAtoms are set by the evidence
-#                 sfVarIdx = len(sf_vars)
-#                 sf_vars.append(var)
-#                 for gndAtom in gndAtoms:
-#                     sf_gndAtom2VarIdx[gndAtom] = sfVarIdx
-#                 sf_varIdx2GndAtoms[sfVarIdx] = gndAtoms
-#                 if varIdx in self.mutexVars:
-#                     sf_mutexVars.add(sfVarIdx)
-#         self.vars = sf_vars
-#         self.gndAtom2VarIndex = sf_gndAtom2VarIdx
-#         self.varIdx2GndAtom = sf_varIdx2GndAtoms
-#         self.mutexVars = sf_mutexVars
-            
-        
-#     @deprecated
-#     def computeDivisor(self):
-#         '''
-#         === DEPRECATED: this functionality has been moved to the WCSP class. ===
-#         Computes a divisor for making all formula weights integers.
-#         '''
-#         # store all weights in a sorted list
-#         weights = []
-#         minWeight = None
-#         for f in self.mln.formulas:
-#             if f.isHard or f.weight == 0.0: continue
-#             w = abs(f.weight)
-#             if w in weights:
-#                 continue
-#             bisect.insort(weights, w)
-#             if minWeight is None or w < minWeight and w > 0:
-#                 minWeight = w
-#         
-#         # compute the smallest difference between subsequent weights
-#         deltaMin = None
-#         w1 = weights[0]
-#         if len(weights) == 1:
-#             deltaMin = weights[0]
-#         for w2 in weights[1:]:
-#             diff = w2 - w1
-#             if deltaMin is None or diff < deltaMin:
-#                 deltaMin = diff
-#             w1 = w2
-# 
-#         divisor = 1.0
-#         if minWeight < 1.0:
-#             divisor *= minWeight
-#         if deltaMin < 1.0:
-#             divisor *= deltaMin
-#         return divisor
-    
-#     @deprecated
-#     def computeHardCosts(self):
-#         '''
-#         === DEPRECATED: this functionality has been moved to the WCSP class. ===
-#         Computes the costs for hard constraints that determine
-#         costs for entirely inconsistent worlds (0 probability).
-#         '''
-#         costSum = long(0)
-#         for f in self.mrf.gndFormulas:
-#             if f.isHard or f.weight == 0.0: continue
-#             cost = abs(long(f.weight / self.divisor))
-#             newSum = costSum + cost
-#             if newSum < costSum:
-#                 raise Exception("Numeric Overflow")
-#             costSum = newSum
-#         top = costSum + 1
-#         if top < costSum:
-#             raise Exception("Numeric Overflow")
-#         if top > WCSPConverter.MAX_COST:
-#             self.log.error('Maximum costs exceeded: %d > %d' % (top, WCSPConverter.MAX_COST))
-#             raise
-#         return long(top)
-#     
-#     @deprecated
-#     def generateEvidenceConstraints(self):
-#         '''
-#         === DEPRECATED ===
-#         Creates a hard constraint for every evidence variable that
-#         could not be eliminated by variable simplification.
-#         '''
-#         constraints = []
-#         for i,e in [(i, e) for i, e in enumerate(self.mrf.evidence) if not e is None]:
-#             gndAtom = self.mrf.gndAtomsByIdx[i]
-#             varIdx = self.gndAtom2VarIndex.get(gndAtom, None)
-#             if varIdx is None: continue
-# #            print i,e
-#             block = self.varIdx2GndAtom[varIdx]
-#             value = block.index(gndAtom)
-#             cost = 0
-#             defCost = self.top
-#             if not e: 
-#                 cost = self.top
-#                 defCost = 0
-#             constraint = Constraint([varIdx], [[value, cost]], defCost)
-#             constraints.append(constraint)
-#         return constraints
+
     
     def convert(self):
         '''
         Performs a conversion from an MLN into a WCSP.
         '''
         wcsp = WCSP()
-        wcsp.domSizes = [len(block.idx2val) for block in self.variables]#[max(2,len(self.varIdx2GndAtom[i])) for i, _ in enumerate(self.vars)]
-
-        log = logging.getLogger('wcsp')
-        logic = self.mrf.mln.logic
-        if log.level == logging.DEBUG:
-            self.mrf.printEvidence()
+        wcsp.domSizes = [len(block.idx2val) for block in self.variables]
+        log = logging.getLogger(self.__class__.__name__)
+        mln = self.mrf.mln
+        logic = mln.logic
         # preprocess the ground formulas
         gfs = []
         for gf in self.mrf.gndFormulas:
-            gf.weight = self.mln.formulas[gf.idxFormula].weight
-            gf.isHard = self.mln.formulas[gf.idxFormula].isHard
+            gf.weight = mln.formulas[gf.idxFormula].weight
+            gf.isHard = mln.formulas[gf.idxFormula].isHard
             if gf.weight == 0 and not gf.isHard:
                 continue
             if gf.weight < 0:
-                f = self.mrf.mln.logic.negation([gf])
+                f = logic.negation([gf])
                 f.weight = abs(gf.weight)
                 f.isHard = gf.isHard
             else: f = gf
             f_ = f.simplify(self.mrf)
-#             print gf.weight
-#             if gf.weight == 100:
-#                 log.error('%s ===> %s' % (str(gf), str(f_)))
             if isinstance(f_, Logic.TrueFalse) or gf.weight == 0 and not gf.isHard:
                 continue
             f_ = f_.toNNF()
             f_.weight = f.weight
             f_.isHard = f.isHard
             gfs.append(f_)
-#         log.info('gfs: ' + str(gfs))
         for f in gfs:
             self.generateConstraint(wcsp, f)
         return wcsp
+
 
     def generateConstraint(self, wcsp, wf):
         '''
         Generates and adds a constraint from a given weighted formula.
         '''
-        log = logging.getLogger('wcsp')
+        #log = logging.getLogger('wcsp')
         idxGndAtoms = wf.idxGroundAtoms()
         varIndices = set(map(lambda x: self.gndAtom2AtomicBlock[x].blockidx, idxGndAtoms))
-        
         varIndices = tuple(sorted(varIndices))
         # collect the constraint tuples
         cost2assignments = self.gatherConstraintTuples(wcsp, varIndices, wf)
@@ -312,10 +169,8 @@ class WCSPConverter(object):
         need to be evaluated. Returns a dictionary mapping the constraint
         costs to the list of respective variable assignments.
         ''' 
-        log = logging.getLogger('wcsp')
+        log = logging.getLogger(self.__class__.__name__)
         logic = self.mrf.mln.logic
-#         log.info(str(formula) + ': ' + (str(formula.weight) if not formula.isHard else '(hard)'))
-        
         # we can treat conjunctions and disjunctions fairly efficiently
         defaultProcedure = False
         conj = logic.isConjunctionOfLiterals(formula)
@@ -342,14 +197,6 @@ class WCSPConverter(object):
                         break
                     varVal = block.gndatoms.index(gndAtom)
                 assignment[varIndices.index(block.blockidx)] = varVal
-#                 varIdx = self.gndAtom2VarIndex[gndAtom]
-#                 if varIdx in self.mutexVars:
-#                     if isinstance(gndLiteral, Logic.GroundLit) and varVal == 0:
-#                         defaultProcedure = True
-#                         break
-#                     varVal = self.varIdx2GndAtom[varIdx].index(gndAtom)
-#                 assignment[varIndices.index(varIdx)] = varVal
-                
             if not defaultProcedure:
                 if formula.isHard and pivot is not None:
                     msg = 'No fuzzy truth values are allowed in hard constraints.'
@@ -374,10 +221,8 @@ class WCSPConverter(object):
                 return {cost: (assignment,), defcost: 'else'}
         if defaultProcedure: 
             # fallback: go through all combinations of truth assignments
-#             domains = [range(len(self.varIdx2GndAtom[i])) if i in self.mutexVars else [0,1] for i,_ in enumerate(self.vars) if i in varIndices]
             domains = [list(b.generateValueTuples()) for b in self.variables]
             cost2assignments = {}
-#             log.debug(formula)
             # compute number of worlds to be examined and print a warning
             # if it exceeds 10,000 poss. worlds
             worlds = 1
@@ -390,15 +235,8 @@ class WCSPConverter(object):
                 for var, assignment in zip(varIndices, c):
                     for atom, val in zip(self.variables[var].gndatoms, assignment):
                         world[atom.idx] = val
-#                     if var in self.mutexVars: # mutex constraint
-#                         world[self.varIdx2GndAtom[var][assignment].idx] = 1
-# #                         print '%s ---> 1' % self.varIdx2GndAtom[var][assignment]
-#                     else:
-#                         world[self.varIdx2GndAtom[var][0].idx] = 1 if assignment > 0 else 0
-#                         print '%s ---> %s' % (self.varIdx2GndAtom[var][0], 1 if assignment > 0 else 0)
                 # the MRF feature imposed by this formula 
                 truth = formula.isTrue(world)
-#                 print truth
                 assert truth is not None
                 assert not (truth > 0 and truth < 1 and formula.isHard)
                 cost = WCSP.TOP if (truth < 1 and formula.isHard) else (1 - truth) * formula.weight
@@ -407,6 +245,7 @@ class WCSPConverter(object):
                 assignments.append(c)
             return cost2assignments
         assert False # unreachable
+        
         
     def forbidGndAtom(self, atom, wcsp, trueFalse=True):
         '''
@@ -423,6 +262,7 @@ class WCSPConverter(object):
         c.addTuple([varVal], self.top)
         wcsp.addConstraint(c)
         
+        
     def getMostProbableWorldDB(self, verbose=False):
         '''
         Returns a Database object with the most probable truth assignment.
@@ -432,7 +272,7 @@ class WCSPConverter(object):
         solution, _ = wcsp.solve(verbose)
         if solution is None:
             log.exception('Knowledge base is unsatisfiable.')
-        resultDB = Database(self.mln)
+        resultDB = Database(self.mrf.mln)
         resultDB.domains = dict(self.mrf.domains)
         resultDB.evidence = dict(self.mrf.getEvidenceDatabase())
         for varIdx, valIdx in enumerate(solution):
@@ -440,12 +280,8 @@ class WCSPConverter(object):
             values = block.idx2val[valIdx]
             for atom, val in zip(block.gndatoms, values):
                 resultDB.evidence[str(atom)] = val
-#             if varIdx in self.mutexVars:
-#                 for v in range(len(self.varIdx2GndAtom[varIdx])):
-#                     resultDB.evidence[str(self.varIdx2GndAtom[varIdx][v])] = 1 if (valIdx == v) else 0
-#             else:
-#                 resultDB.evidence[str(self.varIdx2GndAtom[varIdx][0])] = 1 if (valIdx == 1) else 0
         return resultDB
+
 
     def getPseudoDistributionForGndAtom(self, gndAtom):
         '''
