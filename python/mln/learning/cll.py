97301361f0b97d166c133c2cf90a4fb969e3ca2a
Daniel Nyga <nyga@cs.uni-bremen.de>
2015-01-29 16:24:20 +0100

    removed debug output

--- python/mln/learning/cll.py
+++ python/mln/learning/cll.py
@@ -82,7 +82,6 @@ class CLL(AbstractLearner):
         # create random partition of the ground atoms
         self._createVariables()
         variables = self.atomicVariables
-        log.info('repartitioning %d' % self.repart)
         random.shuffle(variables)
         self.partitions = []
         self.atomIdx2partition = {}
