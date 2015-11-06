f7df022f6b66c0186550a994edb109e1e0629def
Daniel Nyga <nyga@cs.uni-bremen.de>
2015-11-06 15:45:52 +0100

    adaptations in test case

--- test/pracmln-test.sh
+++ test/pracmln-test.sh
@@ -4,7 +4,7 @@ cd $PRACMLN_HOME/test
 echo Copying pracmln temporarily...
 mkdir src
 # rsync -qa .. src --exclude test
-git clone --branch refactoring .. src 
+git clone --branch release .. src 
 docker build -t pracmln/test .
 echo Removing temporary files...
 rm -rf src
