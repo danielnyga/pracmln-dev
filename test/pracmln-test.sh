3f20ee1580017ec0a9d41c98ac64befb03b9c9c3
Daniel Nyga <nyga@cs.uni-bremen.de>
2015-11-10 15:29:48 +0100

    switch for testing locally or remotely

--- test/pracmln-test.sh
+++ test/pracmln-test.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+if [ "$1" = "local" ]
+then
+    REPO=..
+elif [ "$1" = "remote" ]
+then
+	REPO=git@github.com:danielnyga/pracmln
+else
+	echo specify either "local" or "remote"
+	exit -1
+fi
+
+
 cd $PRACMLN_HOME/test
-echo Copying pracmln temporarily...
+echo cloning pracmln temporarily from $REPO
 mkdir src
 # rsync -qa .. src --exclude test
-git clone --branch release .. src 
+git clone --branch release $REPO src 
 docker build -t pracmln/test .
 echo Removing temporary files...
 rm -rf src
