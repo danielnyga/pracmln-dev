28425545ed6f5f2332526a737fb5fa3513f02b31
Mareike <mareikep@tzi.de>
2015-12-21 15:14:17 +0100

    update webmln layout

--- webmln/run.py
+++ webmln/run.py
@@ -1,5 +1,8 @@
 import logging
 from webmln.gui.app import mlnApp
+from werkzeug.serving import run_simple
+from OpenSSL import SSL
+import os
 
 
 def init_app(app):
@@ -17,4 +20,11 @@ init_app(mlnApp.app)
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    mlnApp.app.run(host='0.0.0.0', port=5002, debug=True, threaded=True)
+    if 'PRAC_SERVER' in os.environ and os.environ['PRAC_SERVER'] == 'true':
+        context = SSL.Context(SSL.SSLv23_METHOD)
+        context.use_privatekey_file(os.path.join('../../', 'default.key'))
+        context.use_certificate_file(os.path.join('../../', 'default.crt'))
+        run_simple('0.0.0.0', 5002, mlnApp.app, ssl_context=context)
+    else:
+        mlnApp.app.run(host='0.0.0.0', port=5002, debug=True, threaded=True)
+
