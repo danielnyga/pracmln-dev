e135de3c81226969c74d091101a3bf6ee847244d
Mareike <mareikep@tzi.de>
2016-02-03 12:27:26 +0100

    re-add old server conf

--- webmln/run.py
+++ webmln/run.py
@@ -1,5 +1,6 @@
 import os
 import logging
+from werkzeug.serving import run_simple
 from pracmln import praclog
 from webmln.gui.app import mlnApp
 from tornado.wsgi import WSGIContainer
@@ -37,6 +38,12 @@ if __name__ == '__main__':
         mlnApp.app.config.from_object('configmodule.TestingConfig')
 
         mlnApp.app.run(host='0.0.0.0', port=5002)
+    elif 'PRAC_SERVER' in os.environ and os.environ['PRAC_SERVER'] == 'old':
+        log.debug('Running WEBMLN in server mode')
+
+        certpath = os.path.dirname(os.path.realpath(__file__))
+        context = (os.path.join(certpath, 'default.crt'), os.path.join(certpath, 'default.key'))
+        run_simple('0.0.0.0', 5002, mlnApp.app, ssl_context=context)
     else:
         log.debug('Running WEBMLN in development mode')
 
