44f9afaa16ff40791fbf82c6f2ee965a34f2aa44
Marc Niehaus <mniehaus@informatik.uni-bremen.de>
2016-06-05 20:10:46 +0200

    made rosmln work again; added evidence to query; removed engine param

--- rosmln/scripts/mln_server.py
+++ rosmln/scripts/mln_server.py
@@ -1,50 +1,75 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('rosmln')
 
 from rosmln.srv import *
 from rosmln.msg import *
 import rospy
+import traceback
 
-from mlnQueryTool import MLNInfer
-
-class Storage:
-	inf = None
-	config = None
-
-	def __init__(self, config=None):
-		if (self.__class__.inf is None):
-			self.__class__.inf = MLNInfer()
-
-		if (config is not None):
-			if (config.mlnFiles != '' and config.db != '' and config.db != ''):
-				self.__class__.config = config
-	
-	@staticmethod
-	def getInstance(config=None):
-		s = Storage(config)
-		return (Storage.inf, Storage.config)
-	
+from pracmln.mln.base import MLN
+from pracmln.mln.database import parse_db, Database
+from pracmln.mln.methods import InferenceMethods
 
 
-def handle_mln_query(req):
-	inf, conf = Storage.getInstance(req.config)
-	print conf.mlnFiles
-	results = Storage.inf.run(conf.mlnFiles, conf.db, conf.method, req.query.queries, conf.engine, conf.output_filename,
-				              saveResults=conf.saveResults, maxSteps=5000, logic=conf.logic, grammar=conf.grammar)
-			
-	tuple_list = []
-	for atom, p in results.iteritems():
-		tuple_list.append(AtomProbPair(atom, p))
+class MLNInterfaceServer:
+    def __init__(self):
+        self.__config = None
 
-	return MLNInterfaceResponse(MLNDatabase(tuple_list))
+    def run(self, node_name="rosmln", service_name="mln_interface"):
+        rospy.init_node(node_name)
+        rospy.Service(service_name, MLNInterface, self.__handle_mln_query)
+        rospy.loginfo("MLN is ready to be queried!")
+        rospy.spin()
 
-def mln_interface_server():
-    rospy.init_node('rosmln')
-    s = rospy.Service('mln_interface', MLNInterface, handle_mln_query)
-    print "MLN is ready to be queried."
-    rospy.spin()
+    def __handle_mln_query(self, request):
+        try:
+            rospy.loginfo("Processing request...")
+            config = self.__get_config(request)
+            mln = MLN(config.logic, config.grammar, config.mlnFiles)
+            db = self.__get_db(request, config, mln)
+            materialized_mln = mln.materialize(db)
+            mrf = materialized_mln.ground(db)
+            if not request.query:
+                raise Exception("No query provided!")
+            inference = InferenceMethods.clazz(config.method)(mrf, request.query.queries)
+            result = inference.run()
+            self.__save_results(config, result)
+            self.__config = config
+            tuple_list = []
+            for atom, probability in inference.results.items():
+                tuple_list.append(AtomProbPair(str(atom), float(probability)))
+            to_return = MLNInterfaceResponse(MLNDatabase(tuple_list))
+            rospy.loginfo("Done!")
+            return to_return
+        except Exception:
+            rospy.logfatal(traceback.format_exc())
+            return MLNDatabase([])
+
+    def __get_config(self, request):
+        if self.__config is None and request.config.mlnFiles == "":
+            raise Exception("No configuration provided!")
+        elif request.config.mlnFiles != "":
+            return request.config
+        elif self.__config is not None:
+            return self.__config
+
+    def __get_db(self, request, config, mln):
+        if not request.query.evidence and config.db == "":
+            raise Exception("No evidence provided!")
+        if request.query.evidence and config.db != "":
+            raise Exception("Duplicate evidence; provide either a db in the config or an evidence db in the query")
+        if request.query.evidence:
+            to_return = parse_db(mln, reduce(lambda x, y: x + "\n" + y, request.query.evidence))
+        else:
+            to_return = Database.load(mln, config.db)
+        if len(to_return) != 1:
+            raise Exception("Only one db is supported!")
+        return to_return[0]
+
+    def __save_results(self, config, resutls):
+        if config.saveResults:
+            with open(config.output_filename, "w") as output:
+                resutls.write(output)
+
 
 if __name__ == "__main__":
-    mln_interface_server()
-
-
+    MLNInterfaceServer().run()
